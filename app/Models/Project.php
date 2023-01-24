514de0cc7e7f6031fc7b380e991b1f689347a428
saracassandra7 <saracassandra7@gmail.com>
2023-01-24 16:36:24 +0100

    first commit

---
+++ app/Models/Project.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'slug', 'client_name', 'summary', 'cover_image', 'image_original_name', 'type_id'];
+
+    public function type(){
+        return $this->belongsTo(Type::class);
+    }
+
+    public static function generateSlug($string){
+        $slug = Str::slug($string, '-');
+
+        $original_slug = $slug;
+        $c = 1;
+        $exists = Project::where('slug',$slug)->first();
+        while($exists){
+            $slug = $original_slug . '-' . $c;
+            $exists = Project::where('slug',$slug)->first();
+            $c++;
+        }
+        return $slug;
+    }
+
+}
---
+++ database/seeders/ProjectTypeSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProjectTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $projects = Project::all();
+
+        foreach($projects as $project){
+            $type_id = Type::inRandomOrder()->first()->id;
+            $project->type_id = $type_id;
+            $project->update();
+        }
+    }
+}
